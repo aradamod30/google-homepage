348b02444d196d31375fa5dfb6c61f38ab1d613b
aradamod30 <aradamod30@protonmail.com>
2021-08-24 15:40:26 +0700

    build1

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,12 @@
 import './Navbar.css';
 
 export default function Navbar() {
-	return <>
-		<h1>NAVBAR</h1>
+  return <>
+    <ul className="navbar">
+      <li>Test</li>
+      <li>Test</li>
+      <li>Test</li>
+      <li>Test</li>
+    </ul>
 	</>;
 }
